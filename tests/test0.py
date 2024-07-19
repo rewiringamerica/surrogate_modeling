080d4eea0fe734c63c1bc69a8aed618b637cc54a
Darren Vengroff <darren@rewiringamerica.org>
2024-07-19 07:58:40 -0400

    black

--- tests/test0.py
+++ tests/test0.py
@@ -4,14 +4,18 @@ import unittest
 import sys
 import os
 
+
 class MyTestCase(unittest.TestCase):
     """My test case."""
+
     def test_true(self):
         """Test something that is true."""
         self.assertTrue(True)
 
+
 class MyOtherTestCase(unittest.TestCase):
     """My other test case."""
+
     def test_equals(self):
         """Test some other thing that is true."""
         self.assertEqual(4, 2 + 2)
@@ -21,7 +25,9 @@ if os.environ.get("DATABRICKS_RUNTIME_VERSION", None):
     # If we are developing on databricks we have to manually
     # instatiate a test suite and load all the tests from this module.
     test_suite = unittest.TestSuite()
-    test_suite.addTests(unittest.TestLoader().loadTestsFromModule(sys.modules[__name__]))
+    test_suite.addTests(
+        unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+    )
     unittest.TextTestRunner().run(test_suite)
 elif __name__ == "__main__":
     unittest.main()
