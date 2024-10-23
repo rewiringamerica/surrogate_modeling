f58219781a537393d337c4dd566a0d6e592e399d
mikivee <mikivee>
2024-10-23 13:57:59 +0000

    remove features that cannot be collected

--- notebooks/Feature Importance Analysis.py
+++ notebooks/Feature Importance Analysis.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 # MAGIC %md # Feature Importance Analysis
-# This notebook performs feature importance analysis using CatBoost and SHAP values for heat pump savings across different communities.
+# MAGIC
+# MAGIC This notebook performs feature importance analysis for preducting heat pump cost savings across different GGRF communities. This is done by computing SHAP values on a fitted Catboost model.
 
 # COMMAND ----------
 
@@ -8,7 +9,7 @@
 
 # COMMAND ----------
 
-# Import necessary libraries
+from itertools import chain
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -68,9 +69,6 @@ def get_catboost_shap_values(X_train, y_train, X_test, y_test, categorical_featu
     return shap_values, model_score
 
 
-# COMMAND ----------
-
-
 def plot_shap_values(shap_values, fig_title, gcspath, n_feature_display=None):
     """
     Create and save a bar plot of SHAP values.
@@ -86,88 +84,13 @@ def plot_shap_values(shap_values, fig_title, gcspath, n_feature_display=None):
     shap.plots.bar(shap_values, max_display=n_feature_display, show=False)
     plt.title(fig_title)
     fig = plt.gcf()
-    fig.set_size_inches(9, 9)
+    fig.set_size_inches(12, 9)
     fig.tight_layout()
-    save_figure_to_gcfs(fig=fig, gcspath=gcspath, dpi=200, transparent=True)
+    save_figure_to_gcfs(fig=fig, gcspath=gcspath, dpi=200, transparent=False)
 
 
-# COMMAND ----------
 
-# Define communities and their corresponding GEOIDs
-communities = [
-    ("Pittsburgh", ["42003", "42007"]),
-    ("Rhode Island", ["44001", "44003", "44005", "44007", "44009"]),
-    ("Atlanta", ["13121", "13089", "13067", "13135", "13063"]),
-    ("Denver", ["08001", "08031", "08059", "08005", "08013", "08014", "08035"]),
-]
-
-# Transform the list into a dictionary with modified GEOIDs
-communities_dict = {city: [f"G{geoid[:2]}0{geoid[2:]}0" for geoid in geoids] for city, geoids in communities}
-
-# Create a mapping from GISJOIN to community
-gisjoin_to_community = {gisjoin: community for community, gisjoins in communities_dict.items() for gisjoin in gisjoins}
-
-# COMMAND ----------
-
-# Load and prepare data
-metadata = spark.table("ml.surrogate_model.building_metadata")
-features = spark.table("ml.surrogate_model.building_features").where(F.col("upgrade_id") == 0)
-outputs_savings = (
-    spark.table("building_model.resstock_annual_savings_by_upgrade_enduse_fuel")
-    .where(F.col("end_use") == "total")
-    .where(F.col("upgrade_id") == "11.05")
-    .groupby("building_id")
-    .agg(F.sum("kwh_delta").alias("kwh_delta"), F.sum("cost_delta").alias("cost_delta"))
-)
-
-# COMMAND ----------
-
-# Define target variable and feature groups
-target = "cost_delta"
-
-pkey_features = ["upgrade_id", "building_id"]
-location_features = ["weather_file_city", "climate_zone_moisture", "climate_zone_temp", "weather_file_city_index"]
-other_appliance_features = [
-    "heat_pump_sizing_methodology",
-    "has_heat_pump_dryer",
-    "has_induction_range",
-    "has_methane_gas_appliance",
-    "has_fuel_oil_appliance",
-    "has_propane_appliance",
-    "cooking_range_fuel",
-    "clothes_dryer_fuel",
-]
-additional_metadata = [
-    "ashrae_iecc_climate_zone_2004",
-    "federal_poverty_level",
-    "geometry_building_type_acs",
-    "income",
-    "tenure",
-    "county",
-]
-
-# Prepare the main dataframe for analysis
-df_predict = (
-    features.join(outputs_savings.select("building_id", target), on="building_id")
-    .join(metadata.select("building_id", *additional_metadata), on="building_id")
-    .drop(*pkey_features, *location_features, *other_appliance_features)
-    .where(F.col("heating_appliance_type") != "ASHP")
-    .where(~F.col("is_mobile_home"))
-).toPandas()
-
-# Identify categorical and numeric features
-categorical_features = [k for k, v in df_predict.dtypes.items() if v == "object"]
-numeric_features = [k for k, v in df_predict.dtypes.items() if v != "object" and k != target]
-
-# COMMAND ----------
-
-# Add community information to the dataframe
-df_predict["community"] = df_predict["county"].map(gisjoin_to_community)
-
-# COMMAND ----------
-
-
-def fit_and_plot_shap_values(df, categorical_features, numeric_features, target, title_detail):
+def fit_and_plot_shap_values(df, categorical_features, numeric_features, target, title_detail, n_feature_display=None):
     """
     Fit a CatBoost model, calculate SHAP values, and plot feature importances.
 
@@ -177,6 +100,7 @@ def fit_and_plot_shap_values(df, categorical_features, numeric_features, target,
         numeric_features (list): List of numeric feature names
         target (str): Name of the target variable
         title_detail (str): Detail to be added to the plot title
+        n_feature_display (int, optional): Number of top important features to display
     """
     # Prepare features and target
     X_categorical = df[categorical_features].astype(str)
@@ -196,7 +120,7 @@ def fit_and_plot_shap_values(df, categorical_features, numeric_features, target,
     plot_shap_values(
         shap_values=shap_values,
         fig_title=f"Feature Importances for HP Savings ({title_detail}): R^2 = {model_score:.3f}",
-        n_feature_display=12,
+        n_feature_display=n_feature_display,
         gcspath=FIGPATH / f"mean_shap_values_cost_savings_medhp_{'_'.join(title_detail.lower().split())}.png",
     )
     plt.show()
@@ -204,9 +128,133 @@ def fit_and_plot_shap_values(df, categorical_features, numeric_features, target,
 
 # COMMAND ----------
 
+# Load and prepare data
+# baseline metadata
+metadata = spark.table("ml.surrogate_model.building_metadata")
+# baseline sumo features derived from metadata
+features = spark.table("ml.surrogate_model.building_features").where(F.col("upgrade_id") == 0)
+# total energy and cost savings for med eff hp
+outputs_savings = (
+    spark.table("building_model.resstock_annual_savings_by_upgrade_enduse_fuel")
+    .where(F.col("end_use") == "total")
+    .where(F.col("upgrade_id") == "11.05")
+    .groupby("building_id")
+    .agg(F.sum("kwh_delta").alias("kwh_delta"), F.sum("cost_delta").alias("cost_delta"))
+)
+
+# COMMAND ----------
+
+# Define communities and their corresponding county GEOIDs
+communities = [
+    ("Pittsburgh", ["42003", "42007"]),
+    ("Rhode Island", ["44001", "44003", "44005", "44007", "44009"]),
+    ("Atlanta", ["13121", "13089", "13067", "13135", "13063"]),
+    ("Denver", ["08001", "08031", "08059", "08005", "08013", "08014", "08035"]),
+]
+
+# Convert county geoids into GISJOINs
+communities_dict = {c: [f"G{geoid[:2]}0{geoid[2:]}0" for geoid in geoids] for c, geoids in communities}
+
+# Invert the mapping to get lookup of GISJOIN to community
+gisjoin_to_community = {gisjoin: community for community, gisjoins in communities_dict.items() for gisjoin in gisjoins}
+
+# Map buildings to communities
+community_mapping = F.create_map([F.lit(x) for x in chain(*gisjoin_to_community.items())])
+metadata = metadata.withColumn("community", community_mapping[F.col("county")])
+
+# COMMAND ----------
+
+# Define target variable
+target = "cost_delta"
+
+# All the features we don't want to include
+# these are just the primary key
+pkey_features = ["upgrade_id", "building_id"]
+# these are duplicative of other features so we don't need them
+cz_features = [
+    "climate_zone_moisture",
+    "climate_zone_temp",
+    "weather_file_city_index", 
+    "weather_file_city", 
+]
+# these are only useful for upgrade features or are not relevant for hp upgrades
+other_appliance_features = [
+    "heat_pump_sizing_methodology",
+    "has_heat_pump_dryer",
+    "has_induction_range",
+    "has_methane_gas_appliance",
+    "has_fuel_oil_appliance",
+    "has_propane_appliance",
+    # "cooking_range_fuel",
+    # "clothes_dryer_fuel",
+]
+# features we are not likely to be able to collect data on
+# altho we may be able to collect data on the features these depend on
+# so we want to drop these so those can show
+unknown_features = [
+    'window_ufactor',
+    'window_shgc',
+    'water_heater_recovery_efficiency_ef',
+    'duct_insulation_r_value',
+    'duct_leakage_percentage',
+    'infiltration_ach50',
+    'wall_material',
+    'insulation_wall_r_value',
+    'insulation_foundation_wall_r_value',
+    'insulation_slab_r_value',
+    'insulation_rim_joist_r_value',
+    'insulation_floor_r_value',
+    'insulation_roof_r_value',
+    'insulation_ceiling_r_value',
+    'lighting_efficiency', 
+    'plug_load_percentage',
+    'usage_level_appliances'
+]
+
+# metadata that we want to include that didn't get directly translated into features
+additional_metadata = [
+    "ashrae_iecc_climate_zone_2004",
+    "federal_poverty_level",
+    "geometry_building_type_acs",
+    "income",
+    "tenure",
+    "community",
+    "windows", 
+    "county"
+]
+
+# Prepare the main dataframe for analysis
+df_predict = (
+    features.join(outputs_savings.select("building_id", target), on="building_id")
+    .join(metadata.select("building_id", *additional_metadata), on="building_id")
+    .drop(*pkey_features, *cz_features, *other_appliance_features, *unknown_features)
+    .where(F.col("heating_appliance_type") != "ASHP")
+    .where(~F.col("is_mobile_home"))
+).toPandas()
+
+# Identify categorical and numeric features
+categorical_features = [k for k, v in df_predict.dtypes.items() if v == "object"]
+numeric_features = [k for k, v in df_predict.dtypes.items() if v != "object" and k != target]
+
+# COMMAND ----------
+
 # Perform analysis for each community
 for community_name, df_community in df_predict.groupby("community"):
-    fit_and_plot_shap_values(df_community, categorical_features, numeric_features, target, community_name)
+    fit_and_plot_shap_values(
+        df=df_community,
+        categorical_features=categorical_features,
+        numeric_features=numeric_features,
+        target=target,
+        n_feature_display=15,
+        title_detail=community_name
+    )
 
 # Perform analysis for a national sample
-fit_and_plot_shap_values(df_predict.sample(frac=0.3), categorical_features, numeric_features, target, "National")
+fit_and_plot_shap_values(
+    df=df_predict.sample(frac=0.3),
+    categorical_features=categorical_features,
+    numeric_features=numeric_features,
+    target=target,
+    n_feature_display=15,
+    title_detail="National"
+)
