bd743692fce72a4d70fd001c3a9be6522e97743d
mikivee <mikivee>
2025-03-19 16:29:38 +0000

    clean up megastock

--- scripts/megastock/feature_extract_02.py
+++ scripts/megastock/feature_extract_02.py
@@ -37,8 +37,8 @@ baseline_building_metadata_transformed = feature_utils.transform_building_featur
 
 # Check for differences between the possible values of categorical features in MegaStock and in ResStock training features
 comparison_dict = qa_utils.compare_dataframes_string_values(
-    spark.table(f'ml.surrogate_model.building_features_{CURRENT_VERSION_NUM}').where(F.col('upgrade_id')==0),
-    baseline_building_metadata_transformed)
+    spark.table(f'ml.surrogate_model.building_features_{CURRENT_VERSION_NUM}').where(F.col('upgrade_id').isin([0.01])).drop('building_set'),
+    baseline_building_metadata_transformed.drop('building_set'))
 
 # NOTE: if there are differences, these should be fixed upstream in the creation of 'ml.megastock.building_metadata_*'
 # This may fail on weather city for small megastocks (e.g, 10K)
@@ -48,28 +48,10 @@ assert (
 
 # COMMAND ----------
 
-building_metadata_upgrades = feature_utils.build_upgrade_metadata_table(baseline_building_metadata_transformed)
-
-# COMMAND ----------
-
-building_metadata_upgrades = feature_utils.add_weather_city_index(building_metadata_upgrades)
-
-# COMMAND ----------
-
-building_metadata_applicable_upgrades = feature_utils.drop_non_upgraded_samples(
-    building_metadata_upgrades, check_applicability_logic=False
-)
-
-# COMMAND ----------
-
-# building_metadata_upgrades.count()
-
-# COMMAND ----------
-
-n_building_upgrade_samples = building_metadata_upgrades.count()
+n_building_upgrade_samples = baseline_building_metadata_transformed.count()
 print(n_building_upgrade_samples)
-non_null_df = building_metadata_upgrades.dropna()
-assert non_null_df.count() == n_building_upgrade_samples, "Null values present, run qa_utils.check_for_null_values(building_metadata_upgrades)"
+non_null_df = baseline_building_metadata_transformed.dropna()
+assert non_null_df.count() == n_building_upgrade_samples, "Null values present, run qa_utils.check_for_null_values(baseline_building_metadata_transformed)"
 
 # COMMAND ----------
 
@@ -88,7 +70,7 @@ print(table_name)
 fe.create_table(
     name=table_name,
     primary_keys=["building_id", "upgrade_id", "weather_file_city"],
-    df=building_metadata_upgrades,
-    schema=building_metadata_upgrades.schema,
+    df=baseline_building_metadata_transformed,
+    schema=baseline_building_metadata_transformed.schema,
     description="megastock building metadata features",
 )
